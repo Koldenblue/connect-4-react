487f3d2e37f85e1fe85a30ec2f8a7b91e8daaf84
Kevin Miller <kmillergit@outlook.com>
2020-09-21 21:32:39 -0700

    added express but doesn't work with react

---
+++ server.js
@@ -0,0 +1,27 @@
+// Requiring necessary npm packages
+const express = require("express");
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// Requiring our routes
+// go through api routes first, since app.get("*")" is the last html route
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+
+app.listen(PORT, () => {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
